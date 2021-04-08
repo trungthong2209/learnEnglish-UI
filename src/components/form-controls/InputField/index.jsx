fd40abfce9ed7ab2ad688fd2bd077185aeab6ebb
Xhieu <xuanhieu.le.1999@gmail.com>
2021-04-09 03:27:07 +0700

    catch message error, add pagination, get aip all groups
    bắt nội dung hiển thị lỗi.
    lấy tất cả group từ api.
    thêm phân trang nhóm.

--- src/components/form-controls/InputField/index.jsx
+++ src/components/form-controls/InputField/index.jsx
@@ -1,16 +1,18 @@
-import { TextField } from "@material-ui/core";
-import PropTypes from "prop-types";
-import React from "react";
-import { Controller } from "react-hook-form";
+import { TextField } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Controller } from 'react-hook-form';
 
 InputField.propTypes = {
   form: PropTypes.object.isRequired,
   name: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
+
+  label: PropTypes.string,
+  disabled: PropTypes.bool,
 };
 
 function InputField(props) {
-  const { form, name, label } = props;
+  const { form, name, label, disabled } = props;
   const { errors } = form;
   const hasError = errors[name];
 
@@ -18,26 +20,33 @@ function InputField(props) {
     <Controller
       name={name}
       control={form.control}
-      as={TextField}
-      default={form.defaultValues}
-      margin="normal"
-      variant="outlined"
-      fullWidth
-      label={label}
-      error={!!hasError}
-      helperText={errors[name]?.message}
-      inputProps={{
-        style: {
-          fontFamily: ["Open Sans", "sans-serif"].join(","),
-          fontSize: "2rem",
-        },
-      }}
-      InputLabelProps={{
-        style: {
-          fontFamily: ["Open Sans", "sans-serif"].join(","),
-          fontSize: "2rem",
-        },
-      }}
+      render={({ onChange, onBlur, value, name }) => (
+        <TextField
+          margin="normal"
+          variant="outlined"
+          fullWidth
+          label={label}
+          disabled={disabled}
+          error={!!hasError}
+          helperText={errors[name]?.message}
+          name={name}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+          inputProps={{
+            style: {
+              fontFamily: ["Open Sans", "sans-serif"].join(","),
+              fontSize: "1.6rem",
+            },
+          }}
+          // InputLabelProps={{
+          //   style: {
+          //     fontFamily: ["Open Sans", "sans-serif"].join(","),
+          //     fontSize: "1.6rem",
+          //   },
+          // }}
+        />
+      )}
     />
   );
 }
--- src/feature/components/groups/index.jsx
+++ src/feature/components/groups/index.jsx
@@ -11,9 +11,15 @@ import { makeStyles } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import LocalLibraryIcon from "@material-ui/icons/LocalLibrary";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+import ControlPointIcon from "@material-ui/icons/ControlPoint";
+import ReactPaginate from "react-paginate";
+import groupsApi from "../../../api/groupsApi";
+import GroupList from "./components/groupList";
+import HeaderGroups from "./components/Header";
+import Pagination from "./components/pagination";
+import "./components/pagination/style.css";
 Groups.propTypes = {};
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -23,147 +29,79 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.text.secondary,
     flexWrap: "wrap",
   },
-  toolbar: {
-    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    fontFamily: "'Open Sans', sans-serif",
-    fontSize: "1.6 rem",
-    marginTop: "3rem",
-  },
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
-  },
-  heroButtons: {
-    marginTop: theme.spacing(4),
-  },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-  card: {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-  },
-  cardMedia: {
-    paddingTop: "56.25%", // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
-  fontOpen: {
-    fontFamily: ["Open Sans", "sans-serif"].join(","),
-  },
-  font_title: {
-    fontFamily: ["Open Sans", "sans-serif"].join(","),
-    fontSize: "3rem",
-  },
-  font_head: {
-    fontFamily: ["Open Sans", "sans-serif"].join(","),
-    fontSize: "2rem",
-  },
-  groups: {
-    fontFamily: ["Open Sans", "sans-serif"].join(","),
-    fontSize: "1.6rem",
-    textDecoration: "none",
-    width:"100%",
-    height: "100%",
-  },
+  paginationcss:{
+    margin:"30rem"
+  }
 }));
-
-const groups = [
-  {
-    id: "1",
-    img: "https://source.unsplash.com/random",
-    name: "Nhóm học ngữ pháp",
-  },
-  {
-    id: "2",
-    img: "https://source.unsplash.com/random",
-    name: "Nhóm học nghe",
-  },
-  {
-    id: "3",
-    img: "https://source.unsplash.com/random",
-    name: "Nhóm học ngữ pháp nâng cao",
-  },
-];
 
 function Groups(props) {
   const classes = useStyles();
+
   const [roomName, setRoomName] = useState("Nhóm");
   const handleRoomNameChange = (event) => {
     setRoomName(event.target.value);
   };
 
+  const [groups, setGroups] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [groupsPerPage, setGroupsPerPage] = useState(8);
+
+  useEffect(() => {
+    const params = {
+      _limit: 10,
+    };
+    const fetchGroups = async () => {
+      setLoading(true);
+      const groupList = await groupsApi.getAll(params);
+      setGroups(groupList);
+      setLoading(false);
+    };
+    fetchGroups();
+  }, []);
+
+  // get current groups
+  const pagesVisited = pageNumber * groupsPerPage;
+
+  const currentGroups = groups.slice(
+    pagesVisited,
+    pagesVisited + groupsPerPage
+  );
+  // const [pageNumber, setPageNumber] = useState(0);
+
+  // // Change page
+  // const paginate = (pageNumber) => (pageNumber)
+  const pageCount = Math.ceil(groups.length / groupsPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+  console.log(groups);
   return (
     <React.Fragment>
       <Container>
         <Grid container spacing={3}>
           <Grid item xs={12}>
             <CssBaseline />
-
-            <AppBar className={classes.toolbar} position="relative">
-              <Toolbar>
-                <LocalLibraryIcon
-                  className={classes.icon}
-                  fontSize="large"
-                  color="white"
-                  large
-                />
-                <Typography
-                  className={classes.fontOpen}
-                  className={classes.font_title}
-                  variant="h3"
-                  color="inherit"
-                  noWrap
-                >
-                  NHÓM
-                </Typography>
-              </Toolbar>
-            </AppBar>
+            <HeaderGroups />
             <Paper className={classes.paper}>
-              <Grid container spacing={4}>
-                {groups.map((group) => (
-                  <Grid item key={group.id} xs={12} sm={6} md={4}>
-                    <Card className={classes.card}>
-                      <CardActions>
-                        <Link
-                          to={`/groups/${group.id}`}
-                          size="small"
-                          color="primary"
-                          className={classes.groups}
-                          value={group.id}
-                          onClick={handleRoomNameChange}
-                        >
-                          <CardMedia
-                            className={classes.cardMedia}
-                            image={group.img}
-                            title={group.name}
-                          />
-                          <CardContent className={classes.font_head}>
-                            <Typography
-                              gutterBottom
-                              className={classes.font_head}
-                              variant="h5"
-                              component="h2"
-                            >
-                              {group.name}
-                            </Typography>
-                          </CardContent>
-                        </Link>
-                      </CardActions>
-                    </Card>
-                  </Grid>
-                ))}
-              </Grid>
+              <GroupList
+                groups={currentGroups}
+                handleRoomNameChange={handleRoomNameChange}
+                loading={loading}
+              />
+              <ReactPaginate
+               className={classes.paginationcss}
+                previousLabel={"<"}
+                nextLabel={">"}
+                pageCount={pageCount}
+                onPageChange={changePage}
+                containerClassName={"paginationBttns"}
+                previousLinkClassName={"previousBttn"}
+                nextLinkClassName={"nextBttn"}
+                disabledClassName={"paginationDisabled"}
+                activeClassName={"paginationActive"}
+              />
             </Paper>
           </Grid>
         </Grid>
